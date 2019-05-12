ef19dcc0baa0a918997fa3bdd54e677def9e257d
Nikolay Stanishev <stanishev.nikolay@gmail.com>
2019-05-12 17:03:55 +0300

    Add option for getting product by id

--- project/project.c
+++ project/project.c
@@ -18,8 +18,9 @@ struct products {
 
 int load_products(char*, struct products*);
 int save_products(char*, struct products*);
-void find_last(struct products*, struct products*);
+int find_by_id(struct products*, struct products*, long);
 int add_new_product(struct products*);
+int print_product_with_id(struct products*);
 
 int main() {
     char filename[100];
@@ -32,14 +33,25 @@ int main() {
         perror("malloc");
         return -1;
     }
-    if (load_products(filename, products)) {
+    products->next = NULL;
+    if (load_products(filename, products) == -1) {
         return -1;
     }
+
+    struct products *ip = products->next;
+    while(1) {
+        if (ip == NULL) {
+            break;
+        }
+        printf("%ld - %s\n", ip->val.id, ip->val.name);
+        ip = ip->next;
+    }
+    printf("----------------------------------\n");
 
     int option = 0;
     int exit_flag = 1;
     while(exit_flag) {
-        system("@cls||clear");
+        // system("@cls||clear");
 
         printf("1. Add new product\n");
         printf("2. Change quantity of product\n");
@@ -65,7 +77,9 @@ int main() {
                 /* code */
                 break;
             case 4:
-                /* code */
+                if (print_product_with_id(products)) {
+                    return -1;
+                }
                 break;
             case 5:
                 exit_flag = 0;
@@ -75,6 +89,8 @@ int main() {
         }
 
         printf("Continue");
+        char ch;
+        scanf("%c",&ch);
         getchar();
     }
 
@@ -93,7 +109,8 @@ int load_products(char *filename, struct products *products) {
         return -1;
     }
 
-    struct products *curr_products = products;
+    struct products *prev_products = products;
+    struct products *curr_products = NULL;
     while (1) {
         struct product product;
         
@@ -106,10 +123,16 @@ int load_products(char *filename, struct products *products) {
             return -1;
         }
 
+        curr_products = malloc(sizeof(struct products));
+        if (curr_products == NULL) {
+            perror("malloc");
+            return -1;
+        }
         curr_products->val = product;
-        struct products *next_products = NULL;
-        curr_products->next = next_products;
-        curr_products = curr_products->next;
+        curr_products->next = NULL;
+
+        prev_products->next = curr_products;
+        prev_products = curr_products;
     }
 
     fclose(fp);
@@ -126,27 +149,37 @@ int save_products(char* filename, struct products *products) {
     }
 
     while (1) {
+        products = products->next;
+        if (products == NULL) {
+            break;
+        }
+
         if (fwrite(&products->val, sizeof(struct product), 1, fp) != 1) {
             fclose(fp);
             perror("fwrite");
             return -1;
         }
-
-        if (products->next == NULL) {
-            break;
-        }
-        products = products->next;
     }
 
     return 0;
 }
 
-void find_last(struct products *products, struct products *last_products) {
-    last_products = products;
+int find_by_id(struct products *products, struct products *products_with_id, long id) {
+    struct products *iterate_products = products->next;
+    while(1) {
+        if (iterate_products == NULL) {
+            break;
+        }
 
-    while(last_products->next) {
-        last_products = last_products->next;
+        if (iterate_products->val.id == id) {
+            products_with_id->val = iterate_products->val;
+            return 0;
+        }
+
+        iterate_products = iterate_products->next;
     }
+
+    return -1;
 }
 
 int add_new_product(struct products *products) {
@@ -174,13 +207,10 @@ int add_new_product(struct products *products) {
     product.quantity = quantity;
     strcat(product.date, date);
 
-    struct products *last_products = NULL;
-    last_products = malloc(sizeof(struct products));
-    if (last_products == NULL) {
-        perror("malloc");
-        return -1;
+    struct products *last_products = products;
+    while(last_products->next) {
+        last_products = last_products->next;
     }
-    find_last(products, last_products);
 
     struct products *next_products = NULL;
     next_products = malloc(sizeof(struct products));
@@ -189,7 +219,34 @@ int add_new_product(struct products *products) {
         return -1;
     }
     next_products->val = product;
+    next_products->next = NULL;
+
     last_products->next = next_products;
+
+    return 0;
+}
+
+int print_product_with_id(struct products* products) {
+    long id;
+    printf("Insert id: ");
+    scanf("%ld", &id);
+
+    struct products *products_with_id = NULL;
+    products_with_id = malloc(sizeof(struct products));
+    if (products_with_id == NULL) {
+        perror("malloc");
+        return -1;
+    }
+
+    if (find_by_id(products, products_with_id, id) == -1) {
+        printf("Missing product!\n");
+    } else {
+        printf("id: %ld\n", products_with_id->val.id);
+        printf("name: %s\n", products_with_id->val.name);
+        printf("price: %.2f\n", products_with_id->val.price);
+        printf("quantity: %d\n", products_with_id->val.quantity);
+        printf("date: %s\n", products_with_id->val.date);
+    }
 
     return 0;
 }
